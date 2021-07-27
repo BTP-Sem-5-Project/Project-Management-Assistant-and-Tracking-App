0d5739362b24d0ead10bc4b23ed85f2368a2e687
vijayjoshi16 <joshivijay324@gmail.com>
2021-07-27 14:26:07 +0530

    Minor Changes

---
+++ app/src/main/java/com/example/p_mat/RegisterActivity.java
@@ -0,0 +1,52 @@
+package com.example.p_mat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.p_mat.Models.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity {
+    private DatabaseReference myDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        myDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        String name="Vijay Joshi";
+        String github="https://github.com/vijayjoshi16";
+        String email="joshivijay324@gmail.com";
+        String resume="google.com";
+        String phone="999999999";
+        String address="Mumbai";
+        String qualification="BTECH CSE";
+        int experience=4;
+        List<String> skills = new ArrayList<String>();
+        skills.add("Web");
+        skills.add("Android");
+        skills.add("ML");
+        List<String> achievements = new ArrayList<String>();
+        achievements.add("a1");
+        achievements.add("a2");
+        achievements.add("a3");
+        String id = myDatabase.push().getKey();
+        System.out.println(id);
+        User user = new User(id,name,github,email,resume,phone,address,qualification,experience,skills,achievements);
+        myDatabase.child(id).setValue(user);
+
+
+    }
+}
