c8bba7a185ac20cd7f549ccdf2556eb00c55f972
him <himan@LAPTOP-2LATF76M.local>
2021-07-30 21:42:47 +0530

    registor page  update

--- app/src/main/java/com/example/p_mat/SignInActivity.java
+++ app/src/main/java/com/example/p_mat/SignInActivity.java
@@ -2,13 +2,22 @@ package com.example.p_mat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
     }
+    public void goToRegistraionForm(View v){
+        Intent intent= new Intent(this,Regester.class);
+        startActivity(intent);
+    }
+
 }
