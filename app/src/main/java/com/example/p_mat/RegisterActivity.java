c56b2a29950986ab10afce3f1fc5aa11c997d4cd
vijayjoshi16 <joshivijay324@gmail.com>
2021-07-30 21:09:53 +0530

    Connected firebase realtime database

--- app/src/main/java/com/example/p_mat/RegisterActivity.java
+++ app/src/main/java/com/example/p_mat/RegisterActivity.java
@@ -18,14 +18,12 @@ import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity {
     private DatabaseReference myDatabase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         myDatabase = FirebaseDatabase.getInstance().getReference("users");
-
         String name="Vijay Joshi";
         String github="https://github.com/vijayjoshi16";
         String email="joshivijay324@gmail.com";
@@ -43,9 +41,22 @@ public class RegisterActivity extends AppCompatActivity {
         achievements.add("a2");
         achievements.add("a3");
         String id = myDatabase.push().getKey();
+        System.out.println(myDatabase.child(id));
         System.out.println(id);
         User user = new User(id,name,github,email,resume,phone,address,qualification,experience,skills,achievements);
-        myDatabase.child(id).setValue(user);
+        myDatabase.child(id).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(RegisterActivity.this,"User registered successfully",Toast.LENGTH_SHORT).show();
+                System.out.println("Suceess");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this,"Something went wrong",Toast.LENGTH_SHORT).show();
+                System.out.println(e);
+            }
+        });
 
 
     }
