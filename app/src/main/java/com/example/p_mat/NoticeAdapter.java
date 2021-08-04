c4cdc5b9240c8ed774243bbb19f856b3188bd5ed
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-08-04 09:37:00 +0530

    recycler view of notice board created

---
+++ app/src/main/java/com/example/p_mat/NoticeAdapter.java
@@ -0,0 +1,16 @@
+package com.example.p_mat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>{
+
+
+
+}
+
