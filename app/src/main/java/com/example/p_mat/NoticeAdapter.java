89bd5d321e3669b4bdc070837f6736c44b3d7c14
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-08-04 09:37:40 +0530

    notice adapter created

--- app/src/main/java/com/example/p_mat/NoticeAdapter.java
+++ app/src/main/java/com/example/p_mat/NoticeAdapter.java
@@ -10,7 +10,42 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>{
 
+    private String[] data;
+    public NoticeAdapter(String[] data){
+        this.data = data;
+    }
 
+    @Override
+    public NoticeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.notice_card,parent, false);
+        return new NoticeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoticeViewHolder holder, int position) {
+        String title = data[position];
+        holder.title.setText(title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class NoticeViewHolder extends RecyclerView.ViewHolder{
+        TextView date;
+        TextView subtitle;
+        TextView title;
+        TextView description;
+        public NoticeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            date = (TextView) itemView.findViewById(R.id.noticedateandtime);
+            title = (TextView) itemView.findViewById(R.id.noticetitle);
+            subtitle = (TextView) itemView.findViewById(R.id.noticesubtitle);
+            description = (TextView) itemView.findViewById(R.id.noticedescription);
+        }
+    }
 
 }
 
