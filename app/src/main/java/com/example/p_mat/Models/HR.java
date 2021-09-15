c4d90129eee03f366410d2bbdf2b3fe14261734e
vijayjoshi16 <joshivijay324@gmail.com>
2021-09-15 17:48:30 +0530

    Created add organization

---
+++ app/src/main/java/com/example/p_mat/Models/HR.java
@@ -0,0 +1,30 @@
+package com.example.p_mat.Models;
+
+public class HR {
+    public String name, id;
+
+    public HR(){
+
+    }
+
+    public HR(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
