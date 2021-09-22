7a75da4c7c7f7472bf3e738be2b23b8fa3622e26
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-09-22 16:11:32 +0530

    Organisation Invite Frontend

---
+++ app/src/main/java/com/example/p_mat/InvitationBox.java
@@ -0,0 +1,14 @@
+package com.example.p_mat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvitationBox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.invitation_card);
+    }
+}
