ab9f856a7b3dfc58f8c07f53fa933b89eb00c6e0
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2021-09-30 17:58:47 +0530

    notice fragment is completed and fully functional

--- app/src/main/java/com/example/p_mat/add_new_notice.java
+++ app/src/main/java/com/example/p_mat/add_new_notice.java
@@ -2,6 +2,8 @@ package com.example.p_mat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,21 +12,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Date;
 
 import com.example.p_mat.Models.NoticeHelper;
 import com.example.p_mat.Models.TodoHelper;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class add_new_notice extends AppCompatActivity {
     TextView textView;
-    private  final  String[] whomToShow = new String[] {
-            "All","Personal", "Organization Name", "Member 1", "Member 2"
-    };
+    String[] whomToShow = new String[] {"Default"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_notice);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("FIXED", Context.MODE_PRIVATE);
+        String myproject = sharedPreferences.getString("ORG", "DEFAULT");
+        whomToShow[0] = myproject;
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, whomToShow);
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteNoticeItem);
         textView.setAdapter(adapter);
@@ -41,10 +50,14 @@ public class add_new_notice extends AppCompatActivity {
 
                 String Title = noticeAddTitle.getText().toString(); // Not more than 25 characters
                 String Description = noticeAddDescription.getText().toString(); // Not more than 60 characters
-                String AssignedProject = "id123";
+                String AssignedProject = myproject;
                 if(!Title.equals("")){
                     if(!Description.equals("")){
-                        NoticeHelper noticeHelper = new NoticeHelper("27112001", "0320", Title, Description, AssignedProject);
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+                        SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss");
+                        String currentDate= sdf.format(new Date());
+                        String currentTime = sdf2.format(new Date());
+                        NoticeHelper noticeHelper = new NoticeHelper(currentDate, currentTime, Title, Description, AssignedProject);
                         String id = reference.push().getKey();
                         reference.child(id).setValue(noticeHelper);
                         Toast.makeText(getApplicationContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
